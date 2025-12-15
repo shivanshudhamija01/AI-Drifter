890c0cd32928db662a175a1e3e4e975730985603
shivanshudhamija01 <shivanshud@alantrixgeim.com>
2025-12-15 15:25:58 +0530

    Enemy Movement Issue Resolved

--- Assets/AI Drifter/_Scripts/GamePlay/Enemy/AIDrift.cs
+++ Assets/AI Drifter/_Scripts/GamePlay/Enemy/AIDrift.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyType
+{
+    Mad,
+    Smart,
+    Aggressive
+}
 public class AIDrift : MonoBehaviour
 {
     [SerializeField] private float driftTurnSpeed = 3f;   // how slowly/loosely the AI turns (drift feel)
     [SerializeField] private float maxRotationAngle = 45f; // how much the AI can lean into the turn
     [SerializeField] private Transform target;
+    [SerializeField] private float interval = 0.2f;
+    [SerializeField] private EnemyType enemyType;
     private NavMeshAgent agent;
+    private float timeSincePreviousSetDestination = 0; 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.updateRotation = false;   // IMPORTANT — we rotate manually
-    }
-
-    void Update()
-    {
+        agent.updateRotation = false; 
         agent.SetDestination(target.transform.position);
+    }
+    void Update()
+    {   
+        timeSincePreviousSetDestination += Time.deltaTime;
+        if(timeSincePreviousSetDestination > interval)
+        {
+            timeSincePreviousSetDestination = 0;
+            agent.SetDestination(target.transform.position);
+        }
         // If AI is not moving, no rotation needed
         if (agent.desiredVelocity.sqrMagnitude < 0.001f)
             return;
